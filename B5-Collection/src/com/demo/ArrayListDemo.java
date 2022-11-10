8eb6b0294235e936337d060278addb7766c1e619
NayanRaut09 <nynraut72@gmail.com>
2022-11-10 21:26:27 +0530

    added forEach loop

---
+++ B5-Collection/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+	List<String> list =  Arrays.asList("Nayan","Rohan","Pranav","Mayur","Girish");	
+		
+		//System.out.println(list);
+	
+		for(String s:list){
+			System.out.println(s);
+		}
+		
+		
+	}
+
+}
