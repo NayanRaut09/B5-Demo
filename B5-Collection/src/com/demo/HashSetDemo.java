1045fc9546d752a03fc781c60d61eae08b30bdfb
NayanRaut09 <nynraut72@gmail.com>
2023-03-15 22:26:21 +0530

    added hashset

---
+++ B5-Collection/src/com/demo/HashSetDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+
+		HashSet hashSet = new HashSet<>();
+		hashSet.add(101);
+		hashSet.add(202);
+		hashSet.add(303);
+
+		
+	}
+
+}
