e18c2cc5a51b0d93eca344660e6cfc548620664c
NayanRaut09 <nynraut72@gmail.com>
2023-03-20 09:00:08 +0530

    Added HashMap file

---
+++ B5-Collection/src/com/demo/HashMapDemo.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		map.put("Nayan", 98);
+		map.put("Rohit", 77);
+		map.put("Akshay", 88);
+		map.put("Sejal", 99);
+		
+		System.out.println(map);
+		
+	}
+
+}
